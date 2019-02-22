05ae15a9ea15b654e97ee7162551c364f0aec453
unknown <bram.buysschaert@cubis.be>
2019-02-22 15:24:31 +0100

    Getting structure sorted out for apidev.py

--- apidev.py
+++ apidev.py
@@ -1,6 +1,23 @@
-import oauthlib
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 2019
+
+@author: Bramb
+"""
+
+# Include packages
+from oauthlib import oauth2
+# import requests-oauthlib # Currently not working
 import logging
 import sys
+
+# Enable logging
 log = logging.getLogger('oauthlib')
 log.addHandler(logging.StreamHandler(sys.stdout))
 log.setLevel(logging.DEBUG)
+
+# Start dev
+
+
+if __name__ == '__main__':
+    help(oauth2)
