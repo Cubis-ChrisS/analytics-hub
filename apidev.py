5f61b693cb85c83f2cb302c322690a1fe2388aa1
unknown <bram.buysschaert@cubis.be>
2019-02-22 15:54:00 +0100

    Updated oauthlib_requests import

--- apidev.py
+++ apidev.py
@@ -7,7 +7,7 @@ Created on Fri Feb 22 2019
 
 # Include packages
 from oauthlib import oauth2
-import requests_oauthlib # Currently not working
+from requests_oauthlib import OAuth2Session
 import logging
 import sys
 
@@ -25,4 +25,5 @@ if __name__ == '__main__':
     accestoken_url = 'https://oauthasservices-a4f6d560e.hana.ondemand.com/oauth2/api/v1/token'
     client_id = 'sac-hub-demo'
     client_secret = 'sac-hub-demo'
-    help(oauth2.WebApplicationClient)
+    #help(oauth2.WebApplicationClient)
+    help(OAuth2Session)
