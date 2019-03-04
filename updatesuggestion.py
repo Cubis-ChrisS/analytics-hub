fdfae7959b24fef462fa8cc0132f732f8314e645
brambcubis <47887464+brambcubis@users.noreply.github.com>
2019-03-04 10:59:44 +0100

    Delete updatesuggestion.py
    Replaced by updatesuggestions.py

--- updatesuggestion.py
+++
@@ -1,7 +0,0 @@
-# Include packages
-import apiconnection as app
-
-# Create the updater class and update the store
-updater = app.ConnectSacHub('./credits.dat', './token.dat')
-updater.connect()
-updater.getLiveStore()
